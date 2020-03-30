112fe9e20ec73b6cb5fc8c50d489e91d785293aa
PROX-E <lichlord18@gmail.com>
2020-03-29 19:04:02 -0500

    add mysql files to the database folder

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE `burgers_db`;
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Deluxe Burger');
+INSERT INTO burgers (burger_name) VALUES ('McBurger Burger');
+INSERT INTO burgers (burger_name) VALUES ('SUPER Burger');
+INSERT INTO burgers (burger_name) VALUES ('Issa Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('IDEK Burger');
